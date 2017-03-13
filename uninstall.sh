8ca07c354c817893ee90d04aac826839fe7f1544
Sigma <sigma.e4h@gmail.com>
2017-03-13 13:38:43 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 cd ~
+
+# Uninstall bash-it
+echo "Uninstalling bash-it"
+~/.bash_it/uninstall.sh -s
+
+# Remove symlinks and created directories
+echo "Removing symlinks and created directories"
 rm -rf ~/.bash* ~/.vim* ~/.npm-packages ~/.tmux.conf ~/.npmrc
+
+# Restore old dotfiles
+echo "Restoring old dotfiles"
+mv ~/dotfiles_old/* ~/
+
+# Remove remaining directories
+echo "Removing remaining files"
+rm -rf ~/dot*
