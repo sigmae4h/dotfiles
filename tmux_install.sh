4ddab6bc52d5f963c4b10bec3ad2ded89eb6ae1b
Sigma <sigma.e4h@gmail.com>
2017-06-22 16:56:36 -0400

    Add plugin installation to tmux installer

--- tmux_install.sh
+++ tmux_install.sh
@@ -8,6 +8,7 @@
 if [ ! -d "~/.tmux" ]; then
     echo "Installing Tmux Plugin Manager (tpm)"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
     echo "Adding tmux alias"
     printf '\n# Tmux alias\nalias tmux="tmux -2"\n' >> ~/.bashrc
 fi
