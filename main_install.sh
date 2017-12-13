2a572282d1cfa20d05f4e0e95ab612f0e01a48bb
Sigma <sigma.e4h@gmail.com>
2017-12-12 20:27:04 -0500

    Remove bash-it from main install

--- main_install.sh
+++ main_install.sh
@@ -9,7 +9,7 @@ cd ~/dotfiles
 /bin/bash base_install.sh
 
 ########## Install addons
-/bin/bash bash-it_install.sh
+#/bin/bash bash-it_install.sh
 /bin/bash vim_install.sh
 /bin/bash tmux_install.sh
 
